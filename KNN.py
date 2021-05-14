aad567d587ca9dc0506b579120ec396fe8036309
TM111 <tommasomarigo@hotmail.it>
2021-05-14 18:16:28 +0200

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -4,32 +4,56 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from pandas import DataFrame
 from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
 
-small=1
+small=0
 lenght=20
 random=1
 #PREPARAZIONE TRAIN
 train_path = "D:/Desktop/dataspaces/train.csv"
 train=F.formatDataset(pd.read_csv(train_path, delimiter=","),small,lenght,random)
 for i in range(0,15):
-    F.removeAttributeValue(train,i, "unknown",small)
-F.removeAttribute(train,i, "unknown",small)
+    train=F.removeAttributeValue(train,i, "unknown",0)
 train=F.categoricalToNumeric(train)
-print(train[0])
-train = DataFrame(train,columns=F.getAttributes())
-X_train = train.iloc[:, :-1].values
-y_train = train.iloc[:, 16].values
 
-#PREPARAZIONE TEST
+train = DataFrame(train,columns=F.getAttributes())
+del train["job"]
+del train["marital"]
+del train["day"]
+del train["month"]
+del train["poutcome"]
+X_train = train.iloc[:, :-1].values
+y_train = train.iloc[:, 11].values
+
+#PREPARAZIONE DATASET
 test_path = "D:/Desktop/dataspaces/test.csv"
 test=F.formatDataset(pd.read_csv(test_path, delimiter=","),small,lenght,random)
 for i in range(0,15):
-    F.removeAttributeValue(test,i, "unknown",small)
+    test=F.removeAttributeValue(test,i, "unknown",0)
+test=F.categoricalToNumeric(test)
 test = DataFrame(test,columns=F.getAttributes())
+
+del test["job"]
+del test["marital"]
+del test["day"]
+del test["month"]
+del test["poutcome"]
 X_test = test.iloc[:, :-1].values
-y_test = test.iloc[:, 16].values
+y_test = test.iloc[:, 11].values
 
+#FEATURE SCALING
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
 
+#CLASSIFIER FIT
+classifier = KNeighborsClassifier(n_neighbors=5)
+classifier.fit(X_train, y_train)
 
-
-
+y_pred = classifier.predict(X_test)
+count=0
+for i in y_pred:
+    if(i==1):
+        count=count+1
+print(count)
