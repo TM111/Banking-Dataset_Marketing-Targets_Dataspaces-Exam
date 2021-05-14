79140c541246952a815a6252490e11c6a109c449
TM111 <tommasomarigo@hotmail.it>
2021-05-14 18:16:31 +0200

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -8,22 +8,23 @@ small=0
 lenght=20
 random=0
 #PREPARAZIONE DATASET
-train_path = "D:/Desktop/dataspaces/train.csv"
-train=F.formatDataset(pd.read_csv(train_path, delimiter=","),small,lenght,random)
-for i in range(0,16):
-    F.removeAttributeValue(train,i, "unknown",small)
+dataset_path = "D:/Desktop/dataspaces/bank_full.csv"
+dataset=F.formatDataset(pd.read_csv(dataset_path, delimiter=","),small,lenght,random)
+for i in range(0,len(dataset)):
+    F.removeAttributeValue(dataset,i, "unknown",small)
+
 #MATRICE DI CORRELAZIONE
-train_df = pd.DataFrame({
+dataset_df = pd.DataFrame({
     'age':F.getColumn(train, "age"),
     'balance':F.getColumn(train, "balance"),
     'duration': F.getColumn(train, "duration"),
     'campaign': F.getColumn(train, "campaign"),
     'pdays': F.getColumn(train, "pdays"),
     'previous': F.getColumn(train, "previous"),
-    'class': F.getColumn(train, "Class")
+    'Class': F.getColumn(train, "Class")
 })
 
-corr_df = train_df.corr(method='pearson')
+corr_df = dataset_df.corr(method='pearson')
 plt.figure(figsize=(32, 24))
 sns.set(font_scale=3.5)
 #sns.heatmap(corr_df, annot=True)
@@ -35,7 +36,7 @@ sns.set(font_scale=1.4)
 
 #STATISTIC
 data=[]
-columns=list(train_df.columns)[:-1]
+columns=list(dataset_df.columns)[:-1]
 plt.figure(figsize=(35, 20))
 for att in columns:
     data.append(list(F.getStatistic(train,att).values()))
@@ -51,11 +52,11 @@ sns.set(font_scale=4)
 #BAR CHARTS    (variabili qualitative)
 attributes=["job","marital","education","default","housing","loan","contact","month","poutcome"]
 
-att=attributes[0]
+att=attributes[8]
 wid=0.5
 plt.figure(figsize=(60, 24))
 
-keys,no_list,yes_list=F.getOccurrences(train,att,1,1)
+keys,no_list,yes_list=F.getOccurrences(train,att,0,1)
 plt.bar(keys, no_list,width=wid)
 plt.bar(keys, yes_list, bottom = no_list,width=wid)
 plt.show()
