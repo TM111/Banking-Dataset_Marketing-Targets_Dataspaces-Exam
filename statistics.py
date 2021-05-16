e15172b2d9c606bd70d5be8e00a6e30d67a025af
TM111 <tommasomarigo@hotmail.it>
2021-05-16 12:13:55 +0200

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -2,29 +2,44 @@ import functions as F
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import random as rnd
 
 
-small=0
-lenght=20
-random=0
+small=1
+lenght=500
+random=1
 #PREPARAZIONE DATASET
 dataset_path = "D:/Desktop/dataspaces/bank_full.csv"
-dataset=F.formatDataset(pd.read_csv(dataset_path, delimiter=","),small,lenght,random)
-for i in range(0,len(dataset)):
-    F.removeAttributeValue(dataset,i, "unknown",small)
+if(small==1):
+    if(random==1):
+        n = sum(1 for line in open(dataset_path)) - 1
+        skip = sorted(rnd.sample(range(1,n+1),n-lenght)) 
+        dataset = pd.read_csv(dataset_path,delimiter=";", skiprows=skip)
+    else:
+        dataset=pd.read_csv(dataset_path,delimiter=";", nrows=lenght)
+else:
+    dataset=pd.read_csv(dataset_path,delimiter=";")
+
+dataset.drop(["duration","month","day_of_week",
+              "default","pdays"], axis=1, inplace=True) # elimino attributi
+indexRows=[]
+index=-1
+for row in dataset.iloc:
+    index=index+1
+    if("unknown" in row.values):
+        indexRows.append(index)
+dataset.drop(indexRows , inplace=True) #elimino missing values
+
+dataset = dataset.sample(frac=1).reset_index(drop=True) #mescolo il dataset
+
+
 
 #MATRICE DI CORRELAZIONE
-dataset_df = pd.DataFrame({
-    'age':F.getColumn(train, "age"),
-    'balance':F.getColumn(train, "balance"),
-    'duration': F.getColumn(train, "duration"),
-    'campaign': F.getColumn(train, "campaign"),
-    'pdays': F.getColumn(train, "pdays"),
-    'previous': F.getColumn(train, "previous"),
-    'Class': F.getColumn(train, "Class")
-})
 
-corr_df = dataset_df.corr(method='pearson')
+df=dataset[["age","campaign","previous","emp.var.rate",
+            "cons.price.idx","cons.conf.idx","euribor3m"]]
+
+corr_df = df.corr(method='pearson')
 plt.figure(figsize=(32, 24))
 sns.set(font_scale=3.5)
 #sns.heatmap(corr_df, annot=True)
@@ -32,35 +47,37 @@ sns.set(font_scale=3.5)
 #MATRICE DI DISPERSIONE
 plt.figure(figsize=(32, 24))
 sns.set(font_scale=1.4)
-#sns.pairplot(train_df)
+#sns.pairplot(df)
 
 #STATISTIC
 data=[]
-columns=list(dataset_df.columns)[:-1]
 plt.figure(figsize=(35, 20))
-for att in columns:
-    data.append(list(F.getStatistic(train,att).values()))
-#F.drawTable(data, columns, list(F.getStatistic(train,"age").keys()))
+for att in df.columns:
+    data.append(list(F.getStatistic(df,att).values()))
 
+#F.drawTable(data, list(df.columns), list(F.getStatistic(df,"age").keys()))
 
 
 #BOXPLOT  (cambiare y per ogni attributo)
+attributes=["age","campaign","previous","emp.var.rate",
+            "cons.price.idx","cons.conf.idx","euribor3m"]
+att=attributes[0]
 flierprops = dict(markerfacecolor='0.5', markersize=18,linestyle='none')
 sns.set(font_scale=4)
-#sns.boxplot(x="class", y="duration", linewidth=4,data=train_df, flierprops=flierprops)
+#sns.boxplot(x="y", y=att, linewidth=4,data=dataset, flierprops=flierprops)
 
 #BAR CHARTS    (variabili qualitative)
-attributes=["job","marital","education","default","housing","loan","contact","month","poutcome"]
+attributes=["job","marital","education","housing",
+            "loan","contact","poutcome"]
 
-att=attributes[8]
+att=attributes[2]
 wid=0.5
 plt.figure(figsize=(60, 24))
 
-keys,no_list,yes_list=F.getOccurrences(train,att,0,1)
+keys,no_list,yes_list=F.getOccurrences(dataset,att,1,1)
 plt.bar(keys, no_list,width=wid)
 plt.bar(keys, yes_list, bottom = no_list,width=wid)
 plt.show()
-
 
 
 
