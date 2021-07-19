cfeb5e2fb2c0950fb400318e53a7877fc8ec6488
TM111 <tommasomarigo@hotmail.it>
2021-07-19 16:43:44 +0200

    update

--- RandomForest.py
+++ RandomForest.py
@@ -9,7 +9,7 @@ from sklearn.metrics import roc_curve
 from sklearn.metrics import auc
 import sys
 from mlxtend.plotting import plot_learning_curves
-from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
 
 
 small=1
@@ -55,7 +55,6 @@ val_ratio = 0.15
 kf = KFold(n_splits=int((1-test_ratio)/val_ratio))
 kf.get_n_splits(X_train)
 
-
 accuracies = []
 Criterions=["gini","entropy"]
 Max_depth=[]
@@ -64,44 +63,53 @@ for i in range(2,40):
 min_samples_splits=[]
 for i in range(2,50):
     min_samples_splits.append(i)
+n_estimators=[]
+for i in range(1,10):
+    min_samples_splits.append(i*50)
     
 best_accuracy=0;
 best_C=0
 best_D=0
 best_MSS=0
+best_N=0
 i=-1
 
 if(small):
+    Max_depth=[8]
     min_samples_splits=[5]
-max_i=len(Criterions)*len(Max_depth)*len(min_samples_splits)*int((1-test_ratio)/val_ratio)
+    n_estimators=[100]
+    
+max_i=len(Criterions)*len(Max_depth)*len(min_samples_splits)*len(n_estimators)*int((1-test_ratio)/val_ratio)
 
 for c in Criterions:
     for d in Max_depth:
         for mss in min_samples_splits:
-            clf = tree.DecisionTreeClassifier(criterion=c,max_depth=d,max_features="sqrt",min_samples_split=mss,random_state=0)
-            mean_accuracies=[]
-            for train_index, test_index in kf.split(X_train):# KFold Cross Val
-                i=i+1
-                percentage="Tuning: "+str(int(100*(i)/max_i))+"%"
-                sys.stdout.write('\r'+percentage)
+            for num in n_estimators:
+                clf = RandomForestClassifier(n_estimators=num,criterion=c,max_depth=d,max_features="sqrt",min_samples_split=mss,random_state=0)
+                mean_accuracies=[]
+                for train_index, test_index in kf.split(X_train):# KFold Cross Val
+                    i=i+1
+                    percentage="Tuning: "+str(int(100*(i)/max_i))+"%"
+                    sys.stdout.write('\r'+percentage)
         
-                X_train_tmp, X_val = X_train[train_index], X_train[test_index]
-                y_train_tmp, y_val = y_train[train_index], y_train[test_index]
-                clf = clf.fit(X_train_tmp, y_train_tmp)
-                pred_i = clf.predict(X_val)
-                mean_accuracies.append(1-np.mean(pred_i != y_val))
-            acc=sum(mean_accuracies)/len(mean_accuracies)
-            accuracies.append(acc)
-            if(best_accuracy<acc):
-                best_accuracy=acc
-                best_C=c
-                best_D=d
-                best_MSS=mss
+                    X_train_tmp, X_val = X_train[train_index], X_train[test_index]
+                    y_train_tmp, y_val = y_train[train_index], y_train[test_index]
+                    clf = clf.fit(X_train_tmp, y_train_tmp)
+                    pred_i = clf.predict(X_val)
+                    mean_accuracies.append(1-np.mean(pred_i != y_val))
+                acc=sum(mean_accuracies)/len(mean_accuracies)
+                accuracies.append(acc)
+                if(best_accuracy<acc):
+                    best_accuracy=acc
+                    best_C=c
+                    best_D=d
+                    best_MSS=mss
+                    best_N=num
 sys.stdout.write('\r'+"                                             "+'\r')
 
 
 #TEST
-clf = tree.DecisionTreeClassifier(criterion=best_C,max_depth=best_D,max_features="sqrt",min_samples_split=best_MSS,random_state=0)
+clf = RandomForestClassifier(n_estimators=best_N,criterion=best_C,max_depth=best_D,max_features="sqrt",min_samples_split=best_MSS,random_state=0)
 clf.fit(X_train, y_train)
 pred_i = clf.predict(X_test)
 acc=1-np.mean(pred_i != y_test)
@@ -127,7 +135,7 @@ y_test=y_test.astype('int')
 
 
 #probability=True
-clf = tree.DecisionTreeClassifier(criterion=best_C,max_depth=best_D,max_features="sqrt",min_samples_split=best_MSS,random_state=0,)
+clf = RandomForestClassifier(n_estimators=best_N,criterion=best_C,max_depth=best_D,max_features="sqrt",min_samples_split=best_MSS,random_state=0,)
 clf.fit(X_train, y_train)
 
 y_scores = clf.predict_proba(X_test)
@@ -147,7 +155,7 @@ plt.title('ROC Curve of kNN')
 plt.show()
 
 #LEARNING CURVE
-clf = tree.DecisionTreeClassifier(criterion=best_C,max_depth=best_D,max_features="sqrt",min_samples_split=best_MSS,random_state=0)
+clf = RandomForestClassifier(n_estimators=best_N,criterion=best_C,max_depth=best_D,max_features="sqrt",min_samples_split=best_MSS,random_state=0)
 clf.fit(X_train, y_train)
 plot_learning_curves(X_train, y_train, X_test, y_test, clf)
 plt.show()
